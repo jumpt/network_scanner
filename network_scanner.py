8f4c87bd89d29662319ce0f08504fec2e4a18a5b
al <al@test.com>
2019-04-21 15:34:53 -0400

    update

--- network_scanner.py
+++ network_scanner.py
@@ -5,24 +5,27 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
+    # this creates the arp packet, the object accepts the the IP argument as a variable to be used when creating
+    # the packet, pdst is the field that accepts the IP argument.
     # arp_request.show()
-    # in the lecture he keeps referring to these as packets??? and broadcast/arp_request is combining the packets??
+    # this shows the contents of the arp packet
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # this creates the ether packet, dst is the broadcast MAC address
     broadcast.show()
-    # this creates the appropriate (E.G. DEST of pdst) field when we create the object
-    # print(arp_request.summary())
-    # print (broadcast.summary())
+    # this shows the contents of the broadcast packet
+    print(arp_request.summary())
+    # the scapy.ARP class has a summary method which displays a summary
+    print (broadcast.summary())
+    # the scapy.Ether class has a summary method which displays a summary
     # scapy.ls(scapy.Ether())
-    # lists out all fields for scapy.etheR
+    # lists out all fields available for scapy.etheR
     arp_request_broadcast = broadcast/arp_request
-
-
+    # combines the ether and arp packets into a single packet
 
     print(arp_request_broadcast.summary())
+    # the class has a summary method which displays a summary
     arp_request_broadcast.show()
     # this shows more details about the packet
-
-
 
 scan("10.0.3.0/24")
 
